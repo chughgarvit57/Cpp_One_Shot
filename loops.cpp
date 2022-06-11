7edc32f37f465697bca2f34e13163763e947902a
chughgarvit57 <gavi.chugh@gmail.com>
2022-06-11 23:02:51 +0530

    Loops in c++

---
+++ loops.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int index=0;
+    // While loop: 
+    // while (index<4)
+    // {
+    //     cout<<"We are at index "<<index<<endl;
+    //     index++;
+    // }
+    // int i=1;
+    // do
+    // {
+    //     cout<<"Line will execute once"<<'\n';
+    // } while (i < 1);
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<"Index number: "<<i<<'\n';
+    }
+    
+    
+    
+    return 0;
+}
