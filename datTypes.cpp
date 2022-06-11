766e8be8bce3cc554706acb58dbb871b8c992d3b
chughgarvit57 <gavi.chugh@gmail.com>
2022-06-11 22:11:55 +0530

    Datatypes in c++

---
+++ datTypes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Datatypes in c++ \n";
+    // int char float long double short 
+    int a,b,c;
+    a=5;
+    b=7;
+    c=9;
+    cout<<"Value of a --> "<<a<<endl;
+    cout<<"Value of b --> "<<b<<endl;
+    cout<<"Value of c --> "<<c<<endl;
+    double marksMaths = 2.02587965;
+    cout<<"Marks in maths are "<<marksMaths;
+    // camelCase Notations --> first letter of first word small and then followed by capital first letter for next following words
+    cout<<"The camelcase is example is when we write like this: marksInMaths"<<endl;
+    return 0;
+}
