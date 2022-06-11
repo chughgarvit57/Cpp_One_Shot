10866089108a90d5dda9756f19f0f218ec88b84a
chughgarvit57 <gavi.chugh@gmail.com>
2022-06-11 18:08:02 +0530

    Errors fixed

--- comments.cpp
+++ comments.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main(){
     cout<<"Comments in c++ \n";
-    // cout<<"This is a sinle line comment";
+    //        cout<<"This is a sinle line comment";
 
-    
-    /* cout<<"This"<<endl;
+
+    /*          cout<<"This"<<endl;
     cout<<"is"<<endl;
     cout<<"a"<<endl;
     cout<<"multiline"<<endl;
-    cout<<"comment"<<endl;*/
+    cout<<"comment"<<endl;         */
     return 0;
 }
