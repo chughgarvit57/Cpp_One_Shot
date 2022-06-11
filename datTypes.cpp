7724c8f297e81729c8693867a194d07b34609d72
chughgarvit57 <gavi.chugh@gmail.com>
2022-06-11 22:17:06 +0530

    Float datatype added

--- datTypes.cpp
+++ datTypes.cpp
@@ -12,8 +12,10 @@ int main(){
     cout<<"Value of b --> "<<b<<endl;
     cout<<"Value of c --> "<<c<<endl;
     double marksMaths = 2.02587965;
-    cout<<"Marks in maths are "<<marksMaths;
+    cout<<"Marks in maths are "<<marksMaths<<endl;
     // camelCase Notations --> first letter of first word small and then followed by capital first letter for next following words
     cout<<"The camelcase is example is when we write like this: marksInMaths"<<endl;
+    float percentage = 77.825;
+    cout<<"The percentage of you in Xth class is "<<percentage; 
     return 0;
 }
