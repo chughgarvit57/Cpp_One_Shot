bd7bae0e3b23929c0294633c59f7c00b284095e5
chughgarvit57 <gavi.chugh@gmail.com>
2022-06-11 17:54:50 +0530

    Single and multiline comments

---
+++ comments.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Comments in c++ \n";
+    // cout<<"This is a sinle line comment";
+
+    
+    /* cout<<"This"<<endl;
+    cout<<"is"<<endl;
+    cout<<"a"<<endl;
+    cout<<"multiline"<<endl;
+    cout<<"comment"<<endl;*/
+    return 0;
+}
