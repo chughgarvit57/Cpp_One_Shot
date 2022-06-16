c2f8d0c88df2b72f875a97eef6a4ad070dd3e312
chughgarvit57 <gavi.chugh@gmail.com>
2022-06-16 12:48:26 +0530

    Arrays and strings methods

---
+++ Arrays.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<string> //for implementing string functions
+using namespace std;
+
+int main(){
+    // Arrays 
+    // int arr[4] = {2,4,5,6};
+    // cout<<arr<<"\n"; // will output garbage value to console
+    // cout<<"Element at index 0 is ";
+    // cout<<arr[0]<<endl;
+    // cout<<"Element at index 1 is ";
+    // cout<<arr[1]<<endl;
+    // cout<<"Element at index 2 is ";
+    // cout<<arr[2]<<endl;
+    // cout<<"Element at index 3 is ";
+    // cout<<arr[3]<<endl;
+    // return 0;
+    // Marks of student stored in arrays example 
+    // int marks[6];
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     cout<<"Enter marks of "<<i<<"th student ";
+    //     cin>>marks[i];
+    // }
+
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     cout<<"Marks of "<<i<<"th student is "<<marks[i]<<endl;
+    // }
+    // Multidimensional arrays (2D)
+    // int marksMatrix[4][5] = {
+    //     {20,40,25,36,52},
+    //     {25,14,85,45,65},
+    //     {25,12,20,56,55},
+    //     {50,45,85,69,52}
+    // };
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     for (int j = 0; j < 5; j++)
+    //     {
+    //         cout<<"The scores at "<<i<<","<<j<<" are "<<marksMatrix[i][j]<<"\n";
+    //     }
+        
+    // }
+
+
+    // Printing a star pattern now 
+    /* string starPattern[5][4] = {
+        {"*","*","*","*"},
+        {"*","*","*","*"},
+        {"*","*","*","*"},
+        {"*","*","*","*"},
+        {"*","*","*","*"}
+    };
+    for (string i = 0; i < "5"; i++)
+    {
+        for (string j = 0; j < "4"; j++)
+        {
+            cout<<starPattern[5][4]<<"\n";
+        }
+        
+    } */
+    // int a = 45;
+    // float b = float(45);
+    // cout<<b/40;
+
+    // string name = "Garvit";
+    // cout<<"My name is "<<name<<endl;
+    // cout<<"My name's length is "<<name.length()<<endl;
+    // cout<<"My name's first 3 characters are "<<name.substr(0,3)<<endl;
+    // cout<<"My name is "<<name<<endl;
+
+}    
